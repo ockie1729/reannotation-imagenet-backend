2daa249b418bfd1034ff6c93338b18fa5e00b0d2
mofumofuchan <you_oki1@yahoo.co.jp>
2021-02-25 22:25:37 +0900

    RSpecでFactoryBotを使いやすくするよう修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotのメソッドをクラス名を指定せずに使えるようにする
+  config.include FactoryBot::Syntax::Methods
 end
