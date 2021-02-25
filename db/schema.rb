69d54b0c8769a9e336ece9542eb87fda3f45e7c8
mofumofuchan <you_oki1@yahoo.co.jp>
2021-02-25 21:44:54 +0900

    migrationを1stepロールバック

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_123659) do
-
-  create_table "image_classes", force: :cascade do |t|
-    t.string "name"
-    t.string "synset"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_02_24_144308) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
--- spec/models/image_class_spec.rb
+++ spec/models/image_class_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ImageClass, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with email and password" do
+    user = User.new(
+      email: 'hoge@example.com',
+      password: 'hogehoge',
+    )
+    expect(user).to be_valid
+  end
 end
