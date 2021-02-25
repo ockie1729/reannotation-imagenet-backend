711a69c73a38167a88c226a4e52ef179d6b7e186
mofumofuchan <you_oki1@yahoo.co.jp>
2021-02-25 22:27:14 +0900

    AnnotationLabelモデルのspecを追加

--- spec/factories/annotation_labels.rb
+++ spec/factories/annotation_labels.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :annotation_label do
-    explanation { "MyText" }
+    explanation { "これは説明です." }
   end
 end
--- spec/models/annotation_label_spec.rb
+++ spec/models/annotation_label_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe AnnotationLabel, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with explanation" do
+    annotation_label = build(:annotation_label)
+
+    expect(annotation_label).to be_valid
+  end
+
+  it "is invalid without explanation" do
+    annotation_label = build(:annotation_label)
+    annotation_label.explanation = nil
+
+    expect(annotation_label).to be_invalid
+  end
 end
