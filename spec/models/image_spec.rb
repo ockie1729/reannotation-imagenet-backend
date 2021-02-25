04a1528d88726e1d8a05906a22c8076f1afaf63d
mofumofuchan <you_oki1@yahoo.co.jp>
2021-02-25 22:15:44 +0900

    ImageモデルのSpecを追加

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with url and image" do
+    image_class = ImageClass.new(
+      synset: "n0001",
+      name: "example",
+    )
+    image_class = Image.new(
+      url: "https://example.com/images/example.png",
+      image_class: image_class
+    )
+
+    expect(image_class).to be_valid
+  end
 end
