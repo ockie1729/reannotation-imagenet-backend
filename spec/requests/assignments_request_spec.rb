ca9eddc4f1a85def4cda6c54d977fe4c060adc57
mofumofuchan <you_oki1@yahoo.co.jp>
2021-03-02 22:21:09 +0900

    /assignmentの雛形を作成

---
+++ spec/requests/assignments_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Assignments", type: :request do
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/assignments/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
