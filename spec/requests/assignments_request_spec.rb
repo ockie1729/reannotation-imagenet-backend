973e1efa0c729bedfa93f1be737157c9ba87b090
mofumofuchan <you_oki1@yahoo.co.jp>
2021-03-02 22:27:30 +0900

    GET /assignmentに認証を追加

--- spec/requests/assignments_request_spec.rb
+++ spec/requests/assignments_request_spec.rb
@@ -4,7 +4,10 @@ RSpec.describe "Assignments", type: :request do
 
   describe "GET /show" do
     it "returns http success" do
-      get "/assignments"
+      user = create(:user)
+      auth_token = sign_in_through_api(user)
+
+      get "/assignments", headers: auth_token
       expect(response).to have_http_status(:success)
     end
   end
