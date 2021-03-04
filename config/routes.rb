83b7e90749935b64be7e980add3d06841405d1a5
mofumofuchan <you_oki1@yahoo.co.jp>
2021-03-04 23:59:30 +0900

    routeを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'competitions/next'
   get 'leader_board', to: 'leader_board#index'
   get 'assignment', to: 'assignments#show'  # URLは/assignmentで単数形
+  post 'assignment', to: 'assignments#new'  # URLは/assignmentで単数形
 end
