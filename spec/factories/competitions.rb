9c557faea279e03d02275c14c3070aa0b2b96ac8
mofumofuchan <you_oki1@yahoo.co.jp>
2021-03-07 12:31:37 +0900

    seedを変更

--- spec/factories/competitions.rb
+++ spec/factories/competitions.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :competition do
-    starts_at { "2030-01-01 22:31:32" }
-    ends_at { "2021-01-01 22:31:32" }
+    starts_at { "2000-01-01 22:31:32" }
+    ends_at { "2030-01-01 22:31:32" }
     title { "Title" }
     explanation { "Explanation" }
   end
