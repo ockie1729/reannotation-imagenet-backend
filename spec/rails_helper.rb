e8ecc9d769188e2c706447eaf0a70a351a85b395
mofumofuchan <you_oki1@yahoo.co.jp>
2021-03-11 00:06:26 +0900

    テスト中にbulletを走らせるよう修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,16 @@ RSpec.configure do |config|
 
   # DeviseTokenAuthで認証するためのhelperを使えるようにする
   config.include AuthorizationSpecHelper, type: :request
+
+  # bullet
+  if Bullet.enable?
+    config.before(:each) do
+      Bullet.start_request
+    end
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
 end
