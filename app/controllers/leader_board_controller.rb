98ac6e9168e91bb291d5b72361c45dbadcc2cff0
mofumofuchan <you_oki1@yahoo.co.jp>
2021-04-07 22:45:02 +0900

    ユーザーIDが返却されないバグを修正

--- app/controllers/leader_board_controller.rb
+++ app/controllers/leader_board_controller.rb
@@ -4,7 +4,7 @@ class LeaderBoardController < ApplicationController
   def index
     users = User.all.left_joins(:annotations)
               .group(:id)
-              .select('users.name, COUNT(annotations.id) as count_annotations_id')
+              .select('users.id, users.name, COUNT(annotations.id) as count_annotations_id')
               .order('count_annotations_id desc')
 
     responses = users.map.with_index do |user, i|
